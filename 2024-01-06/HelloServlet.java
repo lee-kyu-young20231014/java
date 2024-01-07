455bbb90f3b480318e9ab14b517b52a01189f060
lee-kyu-young20231014 <147901795+lee-kyu-young20231014@users.noreply.github.com>
2024-01-07 14:22:01 +0900

    Add files via upload

---
+++ 2024-01-06/HelloServlet.java
@@ -0,0 +1,45 @@
+package com.web.myproject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String html = "<html><head>"
+				+ "<meta charset=\"UTF-8\">"    //request 에서 파라메터로 받은 한글깨짐방지
+				+"<h1>안녕하세요</h1>"
+				+ "</head></html>";		
+		response.setContentType("text/html; charset=UTF-8"); // 서블릿에서 한글깨짐 방지
+		response.getWriter().append(html);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
