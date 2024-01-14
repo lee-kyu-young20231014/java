c91f01d7939e5a39f936110425b34adbb8b4f972
lee-kyu-young20231014 <147901795+lee-kyu-young20231014@users.noreply.github.com>
2024-01-14 16:12:47 +0900

    Add files via upload

--- 20240114/sql3.sql
+++ 20240114/sql3.sql
@@ -9,3 +9,16 @@ password varchar(32),
 name varchar(32),
 ts timestamp default current_timestamp
 );
+
+insert into user(id,password,name) values('123@123.com','123','홍길동');
+insert into user(id,password,name) values('456@456.com','456','홍길동2');
+insert into user(id,password,name) values('789@789.com','789','홍길동3');
+
+select * from user;
+
+
+create table if not exists feed(
+id varchar(128) primary key,
+content varchar(4096),
+ts timestamp default current_timestamp
+);
